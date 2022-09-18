4f84a38ce59cbc6eadb453e70390ae5f6ddeb068
xunvyre <victoria@thedavids.us>
2022-09-18 15:36:18 -0400

    add department done

--- utils/department-functions.js
+++ utils/department-functions.js
@@ -13,7 +13,25 @@ const viewDeparts = () =>
 
 const addDepart = () =>
 {
-    console.log('success');
+    inquirer.prompt
+    ([
+        {
+            type: 'input',
+            name: 'department_title',
+            message: `What is the name of the department?`
+        }
+    ])
+    .then(({department_title}) =>
+    {
+        const sql = `INSERT INTO departments (department_title) VALUE (?);`;
+        const params = [department_title];
+
+        db.query(sql, params, (err, res) =>
+        {
+            if (err) throw err;
+            viewDeparts();
+        });
+    });
 };
 
 module.exports = {viewDeparts, addDepart};
