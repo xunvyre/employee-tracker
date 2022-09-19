d9796ed81120d0ad4225f0340150671832a7535a
xunvyre <victoria@thedavids.us>
2022-09-19 14:28:55 -0400

    moved all the functions around bc inquirer wasn't working properly

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const db = require('./db/connection');
 const inquirer = require('inquirer');
 const conTable = require('console.table');
-const {viewEmployees, addEmployee, editEmployee} = require('./utils/employee-functions');
-const {viewRoles, addRole} = require('./utils/role-functions');
-const {viewDeparts, addDepart} = require('./utils/department-functions');
-const {quit} = require('./utils/qol-functions');
 
 function startingPrompt()
 {
@@ -32,7 +28,6 @@ function startingPrompt()
         if (response.choice === `Quit.`)
         {
             quit();
-            return;
         }
         else
         {
@@ -59,7 +54,9 @@ function startingPrompt()
                 case `Add a department.`:
                     addDepart();
                     break;
-            }
+                default:
+                    break;
+            };   
         };
     })
     .catch(err =>
@@ -67,5 +64,198 @@ function startingPrompt()
         console.error(err);
     });
 };
+
+/*============Employee Functions============*/
+
+const viewEmployees = () =>
+{
+    const sql = `SELECT * FROM employees 
+    LEFT JOIN roles ON employees.role_id = roles.role_id 
+    LEFT JOIN departments ON roles.department_id = departments.department_id;`
+
+    db.query(sql, (err, res) =>
+    {
+      if (err) throw err;
+      console.table(res);
+      startingPrompt();
+    });
+};
+
+const addEmployee = () =>
+{
+    inquirer.prompt
+    ([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: `What is the first name of the new employee?`
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: `What is the last name of the new employee?`
+        },
+        {
+            type: 'number',
+            name: 'role_id',
+            message: `What is the ID number of the position of the new employee?`
+        },
+        {
+            type: 'number',
+            name: 'manager_id',
+            message: `What is the ID of the supervisor of this employee?`
+        }
+    ])
+    .then(({last_name, first_name, role_id, manager_id}) =>
+    {
+        const sql = `INSERT INTO employees (last_name, first_name, role_id, manager_id) VALUES (?,?,?,?);`;
+        const params = [last_name, first_name, role_id, manager_id];
+
+        db.query(sql, params, (err, res) =>
+        {
+            if (err) throw err;
+            viewEmployees();
+            startingPrompt();
+        });
+    });
+};
+
+const editEmployee = () =>
+{
+    inquirer.prompt
+    ([
+        {
+            type: 'number',
+            name: 'id',
+            message: `What is the ID of the employee you wish to update?`
+        },
+        {
+            type: 'number',
+            name: 'role_id',
+            message: `What is the ID number of their new position?`
+        },
+        {
+            type: 'number',
+            name: 'manager_id',
+            message: `What is the ID number of their manager? Leave blank if they are now in management.`
+        }
+    ])
+    .then(({id, role_id, manager_id}) =>
+    {
+        const sql = `UPDATE employees SET employees.role_id = ?, employees.manager_id = ? WHERE employees.id = ?;`;
+        const params = [role_id, manager_id, id];
+
+        db.query(sql, params, (err, res) =>
+        {
+            if (err) throw err;
+            viewEmployees();
+            startingPrompt();
+        });
+    })
+};
+
+/*============Role Functions============*/
+
+const viewRoles = () =>
+{
+    db.query(`SELECT * FROM roles LEFT JOIN departments ON roles.department_id = departments.department_id;`, (err, res) =>
+    {
+      if (err) throw err;
+      console.table(res);
+      startingPrompt();
+    });
+};
+
+const addRole = () =>
+{
+    inquirer.prompt
+    ([
+
+        {
+            type: 'number',
+            name: 'role_id',
+            message: `What is the ID number of the position?`
+        },
+        {
+            type: 'input',
+            name: 'role_title',
+            message: `What is the name of the position?`
+        },
+        {
+            type: 'number',
+            name: 'salary',
+            message: `How much does this position make per year?`
+        },
+        {
+            type: 'number',
+            name: 'department_id',
+            message: `What is the ID number of the department this position belongs to?`
+        },
+        {
+            type: 'confirm',
+            name: 'management',
+            message: `Is this a management position?`,
+            default: false
+        }
+    ])
+    .then(({role_id, role_title, salary, department_id, management}) =>
+    {
+        const sql = `INSERT INTO roles (role_id, role_title, salary, department_id, management) VALUE (?,?,?,?,?);`;
+        const params = [role_id, role_title, salary, department_id, management];
+
+        db.query(sql, params, (err, res) =>
+        {
+            if (err) throw err;
+            viewRoles();
+            startingPrompt();
+        });
+    });
+};
+
+/*============Department Functions============*/
+
+const viewDeparts = () =>
+{
+    db.query(`SELECT * FROM departments;`, (err, res) =>
+    {
+      if (err) throw err;
+      console.table(res);
+      startingPrompt();
+    });
+};
+
+const addDepart = () =>
+{
+    inquirer.prompt
+    ([
+        {
+            type: 'input',
+            name: 'department_title',
+            message: `What is the name of the department?`
+        }
+    ])
+    .then(({department_title}) =>
+    {
+        const sql = `INSERT INTO departments (department_title) VALUE (?);`;
+        const params = [department_title];
+
+        db.query(sql, params, (err, res) =>
+        {
+            if (err) throw err;
+            viewDeparts();
+            startingPrompt();
+        });
+    });
+};
+
+/*============QOL Functions============*/
+
+const quit = () =>
+{
+    console.log(`Thank you for using Employee Tracker!`);
+    db.end();
+};
+
+/*============Starting Call============*/
 
 startingPrompt();
